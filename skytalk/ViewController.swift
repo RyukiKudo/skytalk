d0d864311deee8646497ed3a112368322285aba4
ryuki kudo <ryukikudo@ryukinomacbook-pro.local>
2017-02-11 18:18:59 +0900

    Initial Commit

---
+++ skytalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skytalk
+//
+//  Created by ryuki kudo on 2017/02/11.
+//  Copyright © 2017年 ryuki kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
